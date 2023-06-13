4d113fb2c29e1fb620050ecea2a5e84a19fe3a3f
Stephen Lu <stephen@heroai.ca>
2023-06-13 21:53:58 +0800

    dockerize frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import fs from 'fs';
 
-export default defineConfig({
+let httpsConfig =
+	process.env.NODE_ENV === 'development'
+		? {
+				key: fs.readFileSync('./nginx/dev/local.key.pem'),
+				cert: fs.readFileSync('./nginx/dev/local.cert.pem')
+		  }
+		: false;
+
+/** @type {import('vite').UserConfig} */
+const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		strictPort: true,
+		watch: {
+			usePolling: process.env.USE_POLLING
+		},
+		hmr: {
+			clientPort: 5050
+		},
+		host: '0.0.0.0',
+		port: 5050,
+		https: httpsConfig
 	}
-});
+};
+
+export default config;
