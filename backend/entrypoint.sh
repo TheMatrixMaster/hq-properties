6d2229e637fbf95885f8cb3e3951b1e225583c15
Stephen Lu <stephen@heroai.ca>
2023-06-19 18:24:29 +0800

    consume secrets properly

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,11 +3,9 @@
 server="${SERVER:-backend_api}"
 prevent_auto_migrate="${PREVENT_AUTO_MIGRATE:-false}"
 
-POSTGRES_PASSWORD=$(<secrets/POSTGRES_PASSWORD)
+POSTGRES_PASSWORD=$(</run/secrets/POSTGRES_PASSWORD)
 export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 export ROCKET_DATABASES="{db={url=\"${DATABASE_URL}\"}}"
-
-echo $POSTGRES_PASSWORD
 
 if [[ "$server" == "filewatcher" ]]
 then
@@ -15,29 +13,15 @@ then
     echo $DATABASE_URL
 
     rm -rf ./static
-    rm -rf ./secrets
 
     # Start the filewatcher at appropriate path
     ./target/release/filewatcher /home/.watchdir
 
-elif [[ "$server" == "backend_api" ]]
-then
+else
     echo $ROCKET_DATABASES
     echo $DATABASE_URL
-
-    rm -rf ./secrets
 
     # Start the server
     ./target/release/backend_api
 
-elif [ "$prevent_auto_migrate" != true ]
-then
-    echo $ROCKET_DATABASES
-    echo $DATABASE_URL
-    
-    # Run migrations on the database
-    echo ""
-    echo "Auto-running migrations:"
-    diesel database setup
-    diesel migration run
 fi
