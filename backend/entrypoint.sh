ccbdb9c90dfcdef27bbe73b7cb5769f6e41f7952
Stephen Lu <stephen@heroai.ca>
2023-06-19 15:17:52 +0800

    try 5

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,16 +9,30 @@ export ROCKET_DATABASES="{db={url=\"${DATABASE_URL}\"}}"
 
 if [[ "$server" == "filewatcher" ]]
 then
+    echo $ROCKET_DATABASES
+    echo $DATABASE_URL
+
+    rm -rf ./static
+    rm -rf ./secrets
+
     # Start the filewatcher at appropriate path
     ./target/release/filewatcher /home/.watchdir
 
 elif [[ "$server" == "backend_api" ]]
 then
+    echo $ROCKET_DATABASES
+    echo $DATABASE_URL
+    
+    rm -rf ./secrets
+
     # Start the server
     ./target/release/backend_api
 
 elif [ "$prevent_auto_migrate" != true ]
 then
+    echo $ROCKET_DATABASES
+    echo $DATABASE_URL
+    
     # Run migrations on the database
     echo ""
     echo "Auto-running migrations:"
