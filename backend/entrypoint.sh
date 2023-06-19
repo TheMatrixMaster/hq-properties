c50ca92e2456a10eb419d781b725dabc332215ee
Stephen Lu <stephen@heroai.ca>
2023-06-19 13:49:45 +0800

    First try

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,22 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-server="${SERVER:-FLASK}"
+server="${SERVER:-backend_api}"
 prevent_auto_migrate="${PREVENT_AUTO_MIGRATE:-false}"
 
-if [[ "$server" == "FILEWATCHER" ]]
+POSTGRES_PASSWORD=$(<secrets/POSTGRES_PASSWORD)
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+export ROCKET_DATABASES="{db={url=\"${DATABASE_URL}\"}}"
+
+if [[ "$server" == "filewatcher" ]]
 then
     # Start the filewatcher at appropriate path
-    $(./target/release/filewatcher)
+    ./target/release/filewatcher /home/.watchdir
 
-else
-    # Run migrations on the database
-    if [ "$prevent_auto_migrate" != true ]
-    then
-        echo ""
-        echo "Auto-running migrations:"
-        diesel migration run
-    fi
-
+elif [[ "$server" == "backend_api" ]]
+then
     # Start the server
-    $(./target/release/backend_api)
+    ./target/release/backend_api
+
+elif [ "$prevent_auto_migrate" != true ]
+then
+    # Run migrations on the database
+    echo ""
+    echo "Auto-running migrations:"
+    diesel database setup
+    diesel migration run
 fi
